1b9567b0de396d3e6cc7d28b271875fc88b60fd0
PitrsScripts <150464632+PitrsScripts@users.noreply.github.com>
2024-11-23 19:50:56 +0100

    fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Pitrs'
+version '1.0.0'
+
+client_scripts {
+    'client.lua',
+    'config.lua'
+}
+server_scripts {
+    'server.lua',
+    'config.lua'
+}
