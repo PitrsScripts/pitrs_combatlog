3934a38abb10a57560353c7e14db7c9bc6d1fe67
Pitrs <150464632+PitrsScripts@users.noreply.github.com>
2024-12-14 09:27:48 +0100

    config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {}
+Config.Webhook = "https://discord.com/api/webhooks/1306724282944065707/wfy5aDxXJPUZCedXXKhm35NoFXLsdphJejg5Ona0ECdRGKZ0PVu1z6Ped4isvFqZlnRh"
+Config.LogBotName = "Pixel AntiCL Bot"
+Config.DrawingTime = 20*1000 --20 seconds
+Config.TextColor = {r=255, g=255,b=255} -- WHITE (Player Data)
+Config.AlertTextColor = {r=255, g=0, b=0} -- RED (Player Left Game)
+Config.LogSystem = true
+Config.UseSteam = true 
+Config.AutoDisableDrawing = true
+Config.AutoDisableDrawingTime = 15000
