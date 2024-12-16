ca21d437b5c4cfd1f0beb5d220f139baa5f57eb0
Pitrs <150464632+PitrsScripts@users.noreply.github.com>
2024-12-16 01:36:19 +0100

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,6 +1,6 @@
 Config = {}
 Config.Webhook = ""
-Config.LogBotName = "Pixel AntiCL Bot"
+Config.LogBotName = "Combatlog Bot"
 Config.DrawingTime = 20*1000 --20 seconds
 Config.TextColor = {r=255, g=255,b=255} -- WHITE (Player Data)
 Config.AlertTextColor = {r=255, g=0, b=0} -- RED (Player Left Game)
